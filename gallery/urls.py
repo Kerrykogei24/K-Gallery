682dcbfbc4796b170b9d595de31dc2ed108b5806
Kerrykogei24 <kerrykomar.gmail.com>
2021-01-09 18:56:32 +0300

    create admin superuser

--- gallery/urls.py
+++ gallery/urls.py
@@ -5,4 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     url(r'^$',views.pics,name='pics'),
+        url(r'^search/',views.search_results,name = 'search_results'),
+    url(r'^location/(\d+)',views.viewPics_by_location,name='locationpic'),
+    url(r'^category/(\d+)',views.viewPics_by_category, name = 'categorypic')
     ]
