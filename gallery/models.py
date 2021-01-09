4cab0bfab4df52de1c8b5e2d7b92eb4d02c46323
Kerrykogei24 <kerrykomar.gmail.com>
2021-01-09 15:05:02 +0300

    created a saving and deleting function

--- gallery/models.py
+++ gallery/models.py
@@ -7,3 +7,14 @@ class Author(models.Model):
     last_name = models.CharField(max_length=50)
     email = models.EmailField()
     phone_number = models.CharField(max_length=300, blank=True)
+
+
+    def __str__(self):
+        return self.first_name
+
+
+    def save_author(self):
+        self.save()
+
+    def delete_author(self):
+        self.save()
