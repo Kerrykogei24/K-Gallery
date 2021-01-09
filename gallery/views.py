a31a8b7bdd553787dd6f6f8232e2dbf47157caa1
Kerrykogei24 <kerrykomar.gmail.com>
2021-01-09 16:19:23 +0300

    created a view to display pics

--- gallery/views.py
+++ gallery/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render,redirect
-from django.http import HttpResponse
-from .models import Author
+from django.http import HttpResponse,Http404
+from .models import Author,Picture,Category,Location
 
 # Create your views here.
 
-def pic(request):
-    
-    return render(request,'pics.html')
+def pics(request):
+    category = Category.get_categories()
+    pictures = Picture.all_pics()
+    location_pics = Location.get_location()
+
+    return render(request,'pics.html',{'pictures': pictures, 'category': category, 'location_pics':location_pics })
