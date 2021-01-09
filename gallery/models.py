2695b646cdf1e11ecf8884b5a89b5774d28b4962
Kerrykogei24 <kerrykomar.gmail.com>
2021-01-09 15:48:33 +0300

    created a location saving function

--- gallery/models.py
+++ gallery/models.py
@@ -49,5 +49,10 @@ class Picture(models.Model):
     
 class Location(models.Model):
     location_name = models.CharField(max_length=80)
+    def save_location(self):
+        self.save()
+        
+    def __str__(self):
+        return self.location_name 
 
     
